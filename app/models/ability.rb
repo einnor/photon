c774234d4c119b3512f90c948f8e518eb10eacc9
Gideon Kitili <emailkitili@gmail.com>
2014-09-27 12:17:20 +0300

    Eliminate error points

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,72 +21,8 @@ class Ability
       #
       # Rules for Security Concious Models.
       #
-
-      # Chama can only access itself
-      can :read, Chama, :user_id => user.id
-
-      # A Chama can read and update its settings record
-      can :ru, Setting, :chama_id => user.chama.id
-
-      # A Chama can read and update its Service Fee record
-      can :ru, ServiceFee, :chama_id => user.chama.id
-
-      # A Chama can read and update its Message Manager record
-      can :ru, MessageManager, :chama_id => user.chama.id
-
-      # A Chama can read and update its SMS Fee record
-      can :cru, SmsFee, :chama_id => user.chama.id
-
-      # A Chama can create, update, read and delete its Members
-      can :crud, Member, :chama_id => user.chama.id
-
-      #
-      # Rules for Less Security Concious Models
-      #
-
-      # A Chama can create, update, read and delete its Events records
-      can :crud, Event, :chama_id => user.chama.id
-
-      # A Chama can create, update, read and delete its Loans records
-      can :create, Loan
-      can :rud, Loan do | loan |
-        loan.member.chama.id == user.chama.id
-      end
-
-      # A Chama can create, update, read and delete its Loan Repayment records
-      can :create, LoanRepayment
-      can :rud, LoanRepayment do | lr |
-        lr.loan.member.chama.id == user.chama.id
-      end
-
-
-      # A Chama can create, update, read and delete its Messages
-      can :create, Message
-      can :rud, Message do | msg |
-        msg.message_manager.chama.id == user.chama.id
-      end
-
-      # A Chama can create, update, read and delete its Penalties records
-      can :crud, Penalty
-      can :rud, Penalty do | penalty |
-        penalty.member.chama.id == user.chama.id
-      end
-
-      # A Chama can create, update, read and delete its Penalty Repayment records
-      can :create, PenaltyRepayment
-      can :rud, PenaltyRepayment do | pr |
-        pr.penalty.member.chama.id == user.chama.id
-      end
-
-      # A Chama can create, update, read and delete its Remittance records
-      can :create, Remittance
-      can :rud, Remittance do | remittance |
-        remittance.member.chama.id == user.chama.id
-      end
-
-      # A Chama can create, update, read and delete its Withdrawal records
-      can :crud, Withdrawal, :chama_id => user.chama.id
-
+      can :read, :all
+            
     else
       can :read, :none
     end
